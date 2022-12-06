321cadc211789665ceb9e3f769662c3eca55731f
Aung Naing Oo <aungaung20.ab@gmail.com>
2022-12-06 21:05:36 +0630

    video capture step complete

--- FaceRecognization/Form1.Designer.cs
+++ FaceRecognization/Form1.Designer.cs
@@ -56,6 +56,7 @@ namespace FaceRecognization
             this.BtnCapture.TabIndex = 1;
             this.BtnCapture.Text = "Capture";
             this.BtnCapture.UseVisualStyleBackColor = true;
+            this.BtnCapture.Click += new System.EventHandler(this.BtnCapture_Click);
             // 
             // BtnDetectFaces
             // 
--- FaceRecognization/Form1.cs
+++ FaceRecognization/Form1.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace FaceRecognization
@@ -14,7 +13,9 @@ namespace FaceRecognization
     {
         #region Varaiables
 
-
+        private VideoCapture videoCapture = null;
+        private Image<Bgr, Byte> currentFrame = null;
+        private readonly Mat frame = new Mat();
 
         #endregion
         public Form1()
@@ -22,5 +23,27 @@ namespace FaceRecognization
             InitializeComponent();
         }
 
+        private void BtnCapture_Click(object sender, EventArgs e)
+        {
+            videoCapture = new VideoCapture();
+            videoCapture.ImageGrabbed += ProcessFrame;
+            videoCapture.Start();
+
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            // Step 1 : Capture
+            videoCapture.Retrieve(frame, 0);
+            currentFrame = frame.ToImage<Bgr, Byte>().Resize(pictureBox1.Width, pictureBox1.Height, Inter.Cubic);
+
+            // Render the video capture into the Picutre Box
+            System.Drawing.Bitmap bmp;
+            using (var ms = new MemoryStream(currentFrame.Bytes))
+            {
+                bmp = new System.Drawing.Bitmap(ms);
+            }
+            pictureBox1.Image = bmp;
+        }
     }
 }
